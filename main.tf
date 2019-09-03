4a4aeaeeb38babca930ea1dcd712923f7f3fde64
akhan <amaan.ngp@gmail.com>
2019-09-03 18:03:08 +0530

    Added comments for output variables of EC2 & RDS

--- main.tf
+++ main.tf
@@ -202,10 +202,12 @@ resource "null_resource" "wordpress_deployment" {
   }
 }
 
+// Get the public ip of the ec2 instance
 output "wordpress_ec2_public_ip" {
   value = "${aws_instance.wp-ec2.public_ip}"
 }
 
+// Get the RDS connection string of RDS Instance
 output "wordpress_rds_host_endpoint" {
   value = "${aws_db_instance.default.endpoint}"
 }
